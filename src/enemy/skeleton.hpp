28e589c063e9bfbc1de3c812b1803a88b8b845d8
yeetfarm <jyee017@hammer.cs.ucr.edu>
2021-05-15 21:03:04 -0700

    enemy classes

---
+++ src/enemy/skeleton.hpp
@@ -0,0 +1,14 @@
+#ifndef __SKELETON_HPP__
+#define __SKELETON_HPP__
+
+#include <iostream>
+
+#include "character.hpp"
+
+using namespace std;
+
+class Skeleton : public Character{
+    
+};
+#endif //__SKELETON_HPP__
+
---
+++ src/enemy/spider.hpp
@@ -0,0 +1,14 @@
+#ifndef __SPIDER_HPP__
+#define __SPIDER_HPP__
+
+#include <iostream>
+
+#include "character.hpp"
+
+using namespace std;
+
+class Spider : public Character{
+    
+};
+#endif //__SPIDER_HPP__
+
