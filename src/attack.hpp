0383895aae18e5be8decfc9796219b3d1cdcb69a
Lionblaze218 <szeng010@ucr.edu>
2021-05-15 20:29:54 -0700

    Adding the attack, character, and main files. Fixed their inheritance problem

---
+++ src/attack.hpp
@@ -0,0 +1,42 @@
+#ifndef __ATTACK_HPP__
+#define __ATTACK_HPP__
+
+#include <iostream>
+#include "character.hpp"
+
+using namespace std;
+
+class Character;
+
+class AttackStrat{
+public:
+    virtual void attack(Character* character_, Character* enemy) = 0;
+};
+
+class AttackWarrior : public AttackStrat{
+public:
+    virtual void attack(Character* character_, Character* enemy);
+};
+
+class AttackMage : public AttackStrat{
+public:
+    virtual void attack(Character* character_, Character* enemy);
+};
+
+class AttackAssassin : public AttackStrat{
+public:
+    virtual void attack(Character* character_, Character* enemy);
+};
+
+class AttackArcher : public AttackStrat{
+public:
+    virtual void attack(Character* character_, Character* enemy);
+};
+
+class AttackEnemy : public AttackStrat{
+public:
+    virtual void attack(Character* character_, Character* enemy);
+};
+
+#endif //__ATTACK_HPP__
+
---
+++ src/character.hpp
@@ -0,0 +1,50 @@
+#ifndef __CHARACTER_HPP__
+#define __CHARACTER_HPP__
+
+#include <iostream>
+
+#include "attack.hpp"
+
+using namespace std;
+
+class AttackStrat;
+
+class Character{
+protected:
+    int health;
+    int armor;
+    int damage;
+    int speed;
+    AttackStrat* attack_ = nullptr;
+
+public:
+    Character(int health_, int armor_, int damage_, int speed_):health(health_), armor(armor_), damage(damage_), speed(speed_){}
+
+    void setAttack(AttackStrat* attack);
+
+    int getHealth() const;
+
+    int getArmor() const;
+
+    int getDamage() const;
+
+    int getSpeed() const;
+
+    void attackHit(Character* person);
+
+    void expadd(int add);
+
+    void damageHits(int damage_);
+
+    void setHealth(int health_);
+
+    void setArmor(int armor_);
+
+    bool isAlive() const;
+
+    void seeInventory();
+
+    void usePotion();
+
+};
+#endif //__CHARACTER_HPP__
