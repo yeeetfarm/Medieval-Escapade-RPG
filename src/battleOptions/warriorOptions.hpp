06ce06eb6ad960c1a85f297cdbcc1e74e92d9806
Qrooz <cruzcontrol270@gmail.com>
2021-05-17 20:43:06 -0700

    added the battle options hpp files

---
+++ src/battleOptions/warriorOptions.hpp
@@ -0,0 +1,20 @@
+#ifdef __WARRIOROPTIONS_HPP__
+#define __WARRIOROPTIONS_HPP__
+
+#include <iostream>
+#include "character.hpp"
+#include "attack.hpp"
+#include "battleOptions.hpp"
+
+class warriorOptions : public BattleOptions{
+	protected:
+
+	public:
+	//concrete functions implementing warrior's actions.
+	
+
+
+
+};
+
+#endif //__WARRIOROPTIONS_HPP__
