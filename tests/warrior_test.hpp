daf9d27b8880fd4dc1cdf206de9a726a2fed2bc9
Lionblaze218 <szeng010@ucr.edu>
2021-05-29 03:23:28 -0700

    Adding some extra warrior tests, Made archer tests

--- tests/warrior_test.hpp
+++ tests/warrior_test.hpp
@@ -17,5 +17,45 @@ TEST(WarriorTest, BasicTest1){
     EXPECT_DOUBLE_EQ(warr2->getHealth(), 0);
 }
 
+TEST(WarriorTest, GetHealth){
+    Warrior* warr1 = new Warrior(1, 1, 6, 1);
+    AttackWarrior* aWarrior = new AttackWarrior;
+    warr1->setAttack(aWarrior);
+
+    Warrior* warr2 = new Warrior(10, 1, 1, 1);
+
+    warr1->attackHit(warr2);
+
+    ASSERT_EQ(warr2->healthCheck(), true);
+}
+
+TEST(WarriorTest, IsDead){
+    Warrior* warr1 = new Warrior(1, 1, 6, 1);
+    AttackWarrior* aWarrior = new AttackWarrior;
+    warr1->setAttack(aWarrior);
+
+    Warrior* warr2 = new Warrior(6, 1, 1, 1);
+
+    warr1->attackHit(warr2);
+
+    ASSERT_EQ(warr2->isAlive(), false);
+}
+
+TEST(WarriorTest, GetSpeed){
+    Warrior* warr1 = new Warrior(1, 1, 6, 11);
+    AttackWarrior* aWarrior = new AttackWarrior;
+    warr1->setAttack(aWarrior);
+
+    EXPECT_DOUBLE_EQ(warr1->getSpeed(), 11);
+}
+
+TEST(WarriorTest, GetArmor){
+    Warrior* warr1 = new Warrior(1, 3, 6, 11);
+    AttackWarrior* aWarrior = new AttackWarrior;
+    warr1->setAttack(aWarrior);
+
+    EXPECT_DOUBLE_EQ(warr1->getArmor(), 3);
+}
+
 
 #endif
