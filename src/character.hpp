3bc460f82b3f0839c8ffbbef72deb0bff00386a2
Stanton Zeng <44949352+Lionblaze218@users.noreply.github.com>
2021-05-28 17:04:56 -0700

    Update character.hpp

--- src/character.hpp
+++ src/character.hpp
@@ -91,6 +91,7 @@ public:
         this->speed = speed_;
         startingHealth = health_;
     }
+    ~Warrior(){}
     bool healthCheck(){
         if(health < startingHealth/2){
                 return true;
@@ -202,7 +203,6 @@ public:
         return burn;
     }
 };
-/*
   //ENEMIES
 class Dragon : public Character{
     public:
@@ -303,5 +303,4 @@ class Zombies : public Character{
 			this->health += 5;
 		}
 };
-*/
 #endif //__CHARACTER_HPP__
